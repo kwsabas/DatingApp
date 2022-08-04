0adc279f8d96aa2628308d51c06a949c63a3bd9d
kwsabas <kwsabas07@gmail.com>
2022-08-04 13:31:39 +0300

    fully function

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -36,7 +36,7 @@ import { ToastrModule } from 'ngx-toastr';
     FormsModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass: 'tast-bottom-right'
+      positionClass: 'toast-bottom-right'
     })
   ],
   providers: [],
